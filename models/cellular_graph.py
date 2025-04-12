2ad8ebd01e3bb726d363b7846f2be79d84d2dd96
AlbertPec <szyba363@gmail.com>
2025-04-12 18:22:59 +0200

    changed loading mesh from file, added comments and better selection of starting point of simulation

--- models/cellular_graph.py
+++ models/cellular_graph.py
@@ -5,33 +5,35 @@ from scipy.spatial import cKDTree
 from scipy.sparse.csgraph import minimum_spanning_tree
 
 
-class Space:
-    def __init__(self, binary_array, root= (50, 22)):
+class Space: #, the final frontier
+    def __init__(self, binary_array, root):
         self.graph = self.create_graph_from_matrix(binary_array, root)
     def create_graph_from_matrix(self,binary_array,root, return_mst = False):
+        """
+        Creates a graph of CCS from matrix.
+        """
         points = np.argwhere(binary_array == 1)
 
-        # Stwórz k-d tree dla szybkiego wyszukiwania sąsiadów
+        # Create k-d tree for faster neighbour search
         tree = cKDTree(points)
 
-        # Znajdź sąsiadów w promieniu sqrt(2), czyli do 8 sąsiadów
+        # find 8 neighbours of node
         indices = tree.query_ball_point(points, np.sqrt(2))
 
-        # Utwórz graf ważony
+        # Create weighted graph
         G = nx.Graph()
         for i, neighbors in enumerate(indices):
             for j in neighbors:
-                if i != j:  # Unikaj pętli
+                if i != j:
                     p1, p2 = points[i], points[j]
                     weight = np.linalg.norm(p1 - p2)
                     G.add_edge(tuple(p1), tuple(p2), weight=weight)
 
-        # Znajdź minimalne drzewo rozpinające zaczynając od danego korzenia
+
         if not return_mst:
             return G
         mst = nx.minimum_spanning_tree(G)
 
-        # Konwertuj MST na drzewo ukorzenione w zadanym wierzchołku
         if root in mst.nodes:
             mst = nx.bfs_tree(mst, root)
         else:
@@ -41,6 +43,9 @@ class Space:
 
 
     def draw(self):
+        """
+        Uses pyplot to draw the graph
+        """
         pos = {node: (node[1], -node[0]) for node in self.graph.nodes()}
         nx.draw(self.graph, pos, with_labels=False, node_color='red', edge_color='blue', node_size=10, width=0.5)
         plt.show()
--- utils/data_reader.py
+++ utils/data_reader.py
@@ -1,22 +1,50 @@
 import json
 import cv2
 import numpy as np
+from typing import Tuple
 from models.cellular_graph import Space
 
-BASE_NODES_NR = 5997
-BASE_ROOT = (50, 22)
+def load_to_binary_array(path : str = "resources/img_ccs/", nr_of_nodes: int = 1500) -> Tuple[np.ndarray, Tuple[float, float]]:
+    """
+    Loads a binary array representing the cardiac conduction system from an image.
 
-def load_to_binary_array(path: str, nr_of_nodes: int = BASE_NODES_NR) -> np.ndarray:
-    img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+    Args:
+        path (str): Path to the directory containing the **CCS.png** image and **CCS_info.json** metadata file.
+        nr_of_nodes (int): Desired number of nodes to represent the CCS. Used for scaling the image.
+
+    Returns:
+        Tuple[np.ndarray, Tuple[int, int]]:
+            - A binary NumPy array (1: part of CCS, 0: background).
+            - A tuple representing the (y, x) position of the AV node in the binary array.
+    """
+    img = cv2.imread(path + "CCS.png", cv2.IMREAD_GRAYSCALE)
     ccs = (img > 15).astype(np.int_)
 
-    scale = np.sqrt(nr_of_nodes / BASE_NODES_NR) * 0.876 # Co to za liczba???
+    with open(path + "CCS_info.json") as img_info:
+        data = json.load(img_info)
+        base_nr_of_nodes = data["base_nodes_number"]
+        AV_node_position = data["AV_node_position"]
+
+    scale = np.sqrt(nr_of_nodes/base_nr_of_nodes) * ccs.shape[0]/ccs.shape[1]
 
     ccs = ccs.astype(np.float32)
     ccs = cv2.resize(ccs, (0, 0), fx=scale, fy=scale)
 
-    return (ccs > 0).astype(np.int_)
+    AV_node_position = np.multiply(AV_node_position,scale).astype(np.int_)
 
-def img_graph(path: str, nr_of_nodes: int = BASE_NODES_NR) -> Space:
-    scale = np.sqrt(nr_of_nodes / BASE_NODES_NR) * 0.876
-    return Space(load_to_binary_array(path, nr_of_nodes), (BASE_ROOT[0] * scale, BASE_ROOT[1] * scale))
+    return (ccs > 0).astype(np.int_), tuple(AV_node_position)
+
+def img_graph(path : str = "resources/img_ccs/", nr_of_nodes: int = 1500) -> Space:
+    """
+    Loads a binary array representing the cardiac conduction system from an image.
+
+    Args:
+        path (str): Path to the directory containing the **CCS.png** image and **CCS_info.json** metadata file.
+        nr_of_nodes (int): Desired number of nodes to represent the CCS. Used for scaling the image.
+
+    Returns:
+        Space: Class used to represent CCS as graph
+        """
+    binary_array, av_pos = load_to_binary_array(path = path,nr_of_nodes = nr_of_nodes)
+    return Space(binary_array, av_pos)
+
