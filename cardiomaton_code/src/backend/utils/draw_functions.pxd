176c9bad30f0f8c94103b751f3afd85ff8e7d0b7
Michał <michalm12627@gmail.com>
2025-11-01 19:59:24 +0100

    Small clean-up of frame_renderer

--- cardiomaton_code/src/backend/utils/draw_functions.pxd
+++ cardiomaton_code/src/backend/utils/draw_functions.pxd
@@ -1,6 +1,9 @@
 from src.backend.models.cell_state cimport CellStateC
 from src.backend.models.c_cell cimport CCell
 
+"""
+Function pointer type. Used to select the draw function without branching in the loop
+"""
 ctypedef void (*DrawFunc)(unsigned char*, int, CCell*)
 
 cdef void draw_from_state(unsigned char*, int, CCell*)
--- cardiomaton_code/src/frontend/frame_renderer.py
+++ cardiomaton_code/src/frontend/frame_renderer.py
@@ -22,8 +22,6 @@ class FrameRenderer:
             controller : The simulation controller that provides the next simulation frame as a NumPy array.
         """
         self.ctrl = controller
-        self.cmap = ListedColormap(['white', '#f4f8ff', 'yellow', 'red', 'blue', 'green', 'black'])
-        self.norm = BoundaryNorm(np.arange(-0.5, 7.5, 1), self.cmap.N)
 
         self.last_data = None
         self.current_data = None
@@ -42,61 +40,10 @@ class FrameRenderer:
                 int value of the frame counter
         """
         frame, data = self.ctrl.step(if_charged)
-        self.last_data = data
-        self.ctrl.recorder.record((frame, data))
-        return frame, self.render_frame(target_size, self.last_data, if_charged)
-
-    def render_frame(self, target_size, data: Dict[Tuple[int, int], CellDict], show_charge=False) -> QImage:
         self.current_data = data
-        shape = self.ctrl.shape
-        return self._to_pixmap(data, target_size)
-        # if show_charge:
-        #     return self._render_charge(data, shape, target_size)
-        # else:
-        #     return self._render_state(data, shape, target_size)
+        self.ctrl.recorder.record((frame, data))
+        return frame, self._to_pixmap(target_size)
 
-    def _render_charge(self, data: Dict[Tuple[int, int], CellDict], shape: Tuple[int, int], target_size: QSize) -> QPixmap:
-        rgba = np.zeros((shape[0], shape[1], 4), dtype=np.uint8)
-
-        hsv_img = np.zeros((shape[0], shape[1], 3), dtype=np.uint8)
-        for (x, y), cell in data.items():
-            hsv_img[x, y] = self._cell_to_hsv(cell)
-
-        rgb_img = cv2.cvtColor(hsv_img, cv2.COLOR_HSV2RGB)
-        rgba[:, :, :3] = rgb_img
-        rgba[:, :, 3] = 0
-        for (x, y), cell in data.items():
-            rgba[x, y, 3] = 255
-
-        return self._to_pixmap(rgba, target_size)
-
-    def _render_state(self, data: Dict[Tuple[int, int], CellDict], shape: Tuple[int, int], target_size: QSize) -> QPixmap:
-        rgba = np.zeros((shape[0], shape[1], 4), dtype=np.uint8)
-
-        val = np.ones(shape, dtype=np.uint8) * 255
-        for (x, y), cell in data.items():
-            val[x, y] = cell["state_value"]
-
-        rgb = (self.cmap(self.norm(val))[:, :, :3] * 255).astype(np.uint8)
-
-        rgba[:, :, :3] = rgb
-        rgba[:, :, 3] = 0
-        for (x, y), cell in data.items():
-            rgba[x, y, 3] = 255
-
-        return self._to_pixmap(rgba, target_size)
-
-    def _to_pixmap(self, img_array: np.ndarray, target_size: QSize) -> QPixmap:
-        # h, w, _ = img_array.shape
-        # bytes_per_line = 4 * w
-        # qimage = QImage(img_array.data, w, h, bytes_per_line, QImage.Format.Format_RGBA8888)
-        # return QPixmap.fromImage(qimage).scaled(target_size, aspectRatioMode=Qt.AspectRatioMode.KeepAspectRatio)
+    def _to_pixmap(self, target_size: QSize) -> QPixmap:
         return QPixmap.fromImage(self.image).scaled(target_size, aspectRatioMode=Qt.AspectRatioMode.KeepAspectRatio)
 
-    def _cell_to_hsv(self, cell: CellDict) -> Tuple[int, int, int]:
-        # Got to think of a better way to get the gray color
-        if not cell["auto_polarization"] and cell["state_name"] == "POLARIZATION":
-            return 120, 58, 175
-        # Naive linear cast from range -90...30 to 30...0
-        h = max(-cell["charge"] * 0.25 + 7.5, 0)
-        return int(h), 255,255
